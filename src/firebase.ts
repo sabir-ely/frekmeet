9f28da676c8543f7c777d67643f13ba32dbb827a
Sabir el Yahiaoui <sabir.elyahiaoui@gmail.com>
2023-03-27 20:33:18 +0000

    initial commit

---
+++ src/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+
+const app = initializeApp({
+  apiKey: "AIzaSyBMMEi8o1O-8RbPhCCUHm3jEcxZawps1J4",
+
+  authDomain: "frekmeet.firebaseapp.com",
+
+  projectId: "frekmeet",
+
+  storageBucket: "frekmeet.appspot.com",
+
+  messagingSenderId: "269509975",
+
+  appId: "1:269509975:web:9861862271decdd7bdca12",
+});
